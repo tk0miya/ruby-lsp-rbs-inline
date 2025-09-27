c49deb345aee4b8b81e912cd6ef5e0d4b1879561
Takeshi KOMIYA <i.tkomiya@gmail.com>
2025-09-27 10:02:08 +0900

    Initial commit (Just after "bundle gem ruby-lsp-rbs-inline")

---
+++ lib/ruby/lsp/rbs/inline.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "inline/version"
+
+module Ruby
+  module Lsp
+    module Rbs
+      module Inline
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/ruby/lsp/rbs/inline_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ruby::Lsp::Rbs::Inline do
+  it "has a version number" do
+    expect(Ruby::Lsp::Rbs::Inline::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
