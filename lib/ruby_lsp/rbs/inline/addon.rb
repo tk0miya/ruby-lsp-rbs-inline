e608939f2d37739e0067ab34d3bcbf642f4c86eb
Takeshi KOMIYA <i.tkomiya@gmail.com>
2025-09-27 11:15:45 +0900

    Implement skeleton for Ruby LSP addon
    ref: https://shopify.github.io/ruby-lsp/add-ons.html

---
+++ lib/ruby_lsp/rbs/inline/addon.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module RubyLsp
+  module Rbs
+    module Inline
+      class Addon < ::RubyLsp::Addon
+        attr_reader :global_state #: GlobalState
+        attr_reader :message_queue #: Thread::Queue
+
+        # @rbs global_state: GlobalState
+        # @rbs message_queue: Thread::Queue
+        def activate(global_state, message_queue) #: void
+          @global_state = global_state
+          @message_queue = message_queue
+        end
+
+        def deactivate #: void
+        end
+
+        def name #: String
+          "ruby-lsp-rbs_rails"
+        end
+
+        def version #: String
+          VERSION
+        end
+      end
+    end
+  end
+end
