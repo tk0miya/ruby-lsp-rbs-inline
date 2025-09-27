c17fe5741fce8aea66297ea3e8d757ac7ff33ef3
Takeshi KOMIYA <i.tkomiya@gmail.com>
2025-09-27 12:41:46 +0900

    Remove the corresponding RBS file when Ruby files are deleted

--- lib/ruby_lsp/rbs/inline/addon.rb
+++ lib/ruby_lsp/rbs/inline/addon.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "ruby_lsp/addon"
 require_relative "logger"
 
 module RubyLsp
@@ -27,6 +28,50 @@ module RubyLsp
 
         def version #: String
           VERSION
+        end
+
+        # @rbs changes: Array[{ uri: String, type: Integer }]
+        def workspace_did_change_watched_files(changes) #: void
+          changes.each do |change|
+            case change[:type]
+            when FileChangeType::CREATED, FileChangeType::CHANGED
+              # TODO
+            when FileChangeType::DELETED
+              delete_signature(change[:uri])
+            end
+          end
+        end
+
+        private
+
+        # @rbs uri: String
+        def delete_signature(uri) #: void
+          path = uri_to_path(uri)
+          return unless path.extname == ".rb"
+
+          rbs_path = signature_root_dir / path.sub_ext(".rbs")
+          return unless rbs_path.exist?
+
+          rbs_path.delete
+          logger.info("Deleted RBS signature: #{rbs_path}")
+        rescue StandardError => e
+          logger.info("Failed to delete signature for #{path}: #{e.message}")
+        end
+
+        # @rbs @workspace_path: Pathname?
+
+        def workspace_path #: Pathname
+          @workspace_path ||= Pathname.new(global_state.workspace_path)
+        end
+
+        def signature_root_dir #: Pathname
+          workspace_path / "sig/generated"
+        end
+
+        # @rbs uri: String
+        def uri_to_path(uri) #: Pathname
+          path = uri.delete_prefix("file://")
+          Pathname.new(path).relative_path_from(workspace_path)
         end
       end
     end
---
+++ spec/ruby_lsp/rbs/inline/addon_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "language_server-protocol"
+require "ruby_lsp/global_state"
+require "ruby_lsp/rbs/inline/addon"
+require "tempfile"
+
+include LanguageServer::Protocol::Constant # rubocop:disable Style/MixinUsage
+
+RSpec.describe RubyLsp::Rbs::Inline::Addon do
+  describe "#did_change_watched_files" do
+    subject { addon.workspace_did_change_watched_files(changes) }
+
+    before { addon.activate(global_state, message_queue) }
+
+    after { workspace_path.rmtree }
+
+    let(:addon) { described_class.new }
+    let(:global_state) { instance_double(RubyLsp::GlobalState, workspace_path:) }
+    let(:message_queue) { Thread::Queue.new }
+    let(:workspace_path) { Pathname.new(Dir.mktmpdir) }
+
+    context "when file deleted is received" do
+      let(:changes) do
+        [
+          { uri: "file:///#{workspace_path}/path/to/file.rb",
+            type: FileChangeType::DELETED }
+        ]
+      end
+      let(:rbs_path) { workspace_path / "sig/generated/path/to/file.rbs" }
+
+      context "when the RBS file corresponding to the deleted Ruby file exists" do
+        before do
+          rbs_path.parent.mkpath
+          rbs_path.write("") # Create a dummy RBS file
+        end
+
+        it "deletes the corresponding RBS file" do
+          subject
+
+          expect(rbs_path).not_to exist
+        end
+      end
+
+      context "when the RBS file corresponding to the deleted Ruby file does not exist" do
+        it "does not raise an error" do
+          subject
+
+          expect(rbs_path).not_to exist
+        end
+      end
+    end
+  end
+end
